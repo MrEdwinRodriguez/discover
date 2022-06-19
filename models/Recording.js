c03ae04d2b3f9b7f3e32db1550e4fa83dd1b956b
Edwin <mredwinrodriguez@gmail.com>
2022-06-19 17:42:08 -0400

    initial models

---
+++ models/Recording.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const actionSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'users'},
+    action: {type: String, enum: ['like', 'dislike']},
+})
+
+const commentSchema = new Schema({
+	user: { type: Schema.Types.ObjectId, ref: 'users'},
+	text: {type: String, required: true},
+	name: {type: String },
+    actions: [actionSchema],
+}, {
+    timestamps: true
+});
+
+const RecordingSchema = new Schema ({
+	user: { type: Schema.Types.ObjectId, ref: 'users'},
+    profile: { type: Schema.Types.ObjectId, ref: 'profile'},
+    recordingLink: {type: String},
+    description: { type: String, required: true },
+    rating: { type: Number }, //To be used to determing R/ PG etc
+    title: {type: String },
+    views: {type: Number },
+    actions: [actionSchema],
+	comments: [commentSchema],
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model("Recording", RecordingSchema);
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, //, required: true
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true }, //, required: true
+    // country: { type: Number, ref: 'Country' },
+    status: {type: String, enum: ['Inactive', 'Active'], default: "Active"},
+    is_admin: { type: Boolean, default: false },
+    reset_password_token: String,
+    reset_password_expires: Date,
+    last_login:  { type: Date, default: Date.now },
+  }, {
+    timestamps: true
+  });
+
+module.exports = mongoose.model("User", UserSchema);
+
