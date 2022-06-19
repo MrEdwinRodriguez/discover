777791bd5ffa6952cc430dfcb0b0109579464c61
Edwin <mredwinrodriguez@gmail.com>
2022-06-19 17:52:28 -0400

    setup users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+	try {
+		const users = await User.find().exec();
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(users);
+	} catch (error) {
+		next(err);
+	};
+});
+
+router.post('/signup', async (req, res, next) => {
+	try {
+		//TODO register
+		user = 'test'
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(user);
+	} catch (error) {
+		next(err);
+	}
+});
+
+router.post('/login', (req, res) => {
+	//TODO login
+	//get Token
+});
+
+router.get('/logout', (req, res, next) => {
+	//todo login
+	//clear cookie
+	//
 });
 
 module.exports = router;
