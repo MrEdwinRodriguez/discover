03150c5c700618b486b4ee205d68b36647e9d65d
Edwin <mredwinrodriguez@gmail.com>
2022-08-02 20:23:54 -0400

    login

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap';
 
 const Landing = () => {
   return (
-    <div className="text-center login">
-      <main className="form-signin w-100 m-auto">
-        <form>
-          <img className="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-            <label for="floatingInput">Email address</label>
+    <div className='content'>
+      <div className='content-fluid'>
+        <div className='lock-container login-container'>
+          <div className="text-center loginForm">
+            <main className="form-signin w-100 m-auto">   
+              <img className="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+              <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+              <Form>
+                <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="examplePassword">Password</Label>
+                  <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+                </FormGroup>
+                <Button className="btn btn-primary login-submit">Submit</Button><br></br>
+                <FormGroup>
+                <a href="#" class="forgot-password">Forgot password?</a>
+                </FormGroup>
+              </Form>
+            </main>
           </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-            <label for="floatingPassword">Password</label>
-          </div>
-
-          <div className="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me" /> Remember me
-            </label>
-          </div>
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-        </form>
-      </main>
+        </div>
+      </div>
     </div>
+    
   )
 }
 
