97de93cce5c46e11ed0c47518fe234532c6611fa
Edwin <mredwinrodriguez@gmail.com>
2022-08-18 07:02:35 -0400

    add links to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-subnav navbar-static-top margin-bottom-none" role="navigation">
@@ -11,9 +12,10 @@ const Navbar = () => {
           </div>
           <div className="collapse navbar-collapse" id="subnav">
             <ul className="nav navbar-nav ">
-              <li className="active"><a href="index.html"><i className="fa fa-fw icon-ship-wheel"></i> Timeline</a></li>
-              <li><a href="user-public-profile.html"><i className="fa fa-fw icon-user-1"></i> About</a></li>
-              <li><a href="user-public-users.html"><i className="fa fa-fw fa-users"></i> Friends</a></li>
+              <li className="active"><Link to="/"><i className="fa fa-fw icon-ship-wheel"></i> Timeline</Link></li>
+              <li><Link to="/about"><i className="fa fa-fw icon-user-1"></i> About</Link></li>
+              <li><Link to="/friends"><i className="fa fa-fw fa-users"></i> Friends</Link></li>
+              <li><Link to="/record" type='button'><i class="fa fa-microphone" aria-hidden="true"></i> Record</Link></li>
             </ul>
             <ul className="nav navbar-nav hidden-xs navbar-right ">
               <li><a href="#" data-toggle="chat-box">Chat <i className="fa fa-fw fa-comment-o"></i></a></li>
