6ba1e1b528f4487724a42926d5ec5fd42b566347
Edwin <mredwinrodriguez@gmail.com>
2022-09-23 07:44:08 -0400

    updated recording from class to function

--- client/src/features/recordings/Recording.js
+++ client/src/features/recordings/Recording.js
@@ -1,57 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {saveRecording } from './recordingSlice';
+import { useDispatch } from 'react-redux';
 import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
- 
-class Recording extends Component {
-  constructor(props) {
-    super(props)
- 
-    this.state = {
-      recordState: null,
-      show: false,
-      url: null
-    }
-  }
- 
-  start = () => {
-    this.setState({
-      recordState: RecordState.START
-    })
-  }
- 
-  stop = () => {
-    this.setState({
-      recordState: RecordState.STOP
-    })
-    this.setState({
-      show: true
-    })
-    // this.setState({
-    //   show: true
-    // })
-  }
- 
-  //audioData contains blob and blobUrl
-  onStop = (audioData) => {
-    console.log('audioData', audioData)
-    this.setState({
-      url: audioData.url
-    })
 
-  }
- 
-  render() {
-    const { recordState, show, url } = this.state
- 
-    return (
-      <div>
-        <AudioReactRecorder state={recordState} onStop={this.onStop} />
-        <button onClick={this.start}>Start</button>
-        <button onClick={this.stop}>Stop</button>
-        { show && url ? <audio id='audio' controls src={url} /> : ""}
-      </div>
-    )
-  }
+const Recording = () => {
+	const [recordState, setRecordState] = useState(null);
+	const [show, setShow] = useState(false);
+	const [url, setUrl] = useState(null);
+	const dispatch = useDispatch();
+
+	const start = () => {
+		setRecordState(RecordState.START)
+	};
+
+	const stop = () => {
+		setRecordState(RecordState.STOP);
+		setShow(true);
+	}
+	//audioData contains blob and blobUrl
+	const onStop = (audioData) => {
+		console.log('audioData', audioData);
+		setUrl(audioData.url)
+		dispatch(saveRecording(audioData));
+	}
+  	return (
+		<div>
+			<AudioReactRecorder state={recordState} onStop={onStop} />
+			<button onClick={start}>Start</button>
+			<button onClick={stop}>Stop</button>
+			{ show && url ? <audio id='audio' controls src={url} /> : ""}
+		</div>
+	)
 }
 
 export default Recording
